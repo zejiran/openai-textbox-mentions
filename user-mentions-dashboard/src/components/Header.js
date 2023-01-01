83c170a015e5f26bf5fe840d8a1df1b6cfe53465
Juan Alegría <juanszalegria@gmail.com>
2022-12-31 19:24:40 -0500

    Feat: add header component

---
+++ user-mentions-dashboard/src/components/Header.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Lottie from 'lottie-react';
+import aiLogo from './../assets/ai-logo.json'
+
+export default function Header() {
+    return (
+        <div className="text-container">
+            <Lottie animationData={aiLogo} loop={true} className='lottie' />
+            <h1>OpenAI Text Box Mentions</h1>
+            <p className='regular-text'>This is an OpenAI GPT generator for storing mock
+                users in Elasticsearch and show them in a React mentions style text box.</p>
+        </div>
+    );
+}
---
+++ user-mentions-dashboard/src/components/LastCreatedUsers.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function LastCreatedUsers() {
+    return (
+        <div>
+
+        </div>
+    );
+}
